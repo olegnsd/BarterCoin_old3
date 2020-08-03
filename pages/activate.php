88f34b959ef680ffd27a550dbf8ad7cb172c07b9
Ilya <ilya.neba@gmail.com>
2020-08-03 14:38:36 +0300

    International phones support

--- pages/activate.php
+++ pages/activate.php
@@ -1,29 +1,44 @@
-<div class="pageTitle">
-<h2>Активация карты</h2>
-    					
-    				</div>
+<div class="pageTitle"><h2>Активация карты</h2></div>
 
+<script src="/js/jquery.inputmask.min.js" type="text/javascript"></script>
+<script src="/js/jquery.inputmask.bundle.min.js" type="text/javascript"></script>
 <script type="text/javascript">
-
-jQuery(function($) {
-
-$.mask.definitions['~']='[+-]';
-//
-$('[name=fromnum]').mask('9999 9999 9999 9999',{completed:function(){$('[name=frommonth]').focus();}});
-
-$('[name=frommonth]').mask('99',{completed:function(){$('[name=fromyear]').focus();}});
-
-$('[name=fromyear]').mask('99',{completed:function(){$('[name=fromcvc]').focus();}});
-
-$('[name=fromcvc]').mask('999',{completed:function(){$('input[name=lastname]').focus();}});
-$('[name=phone]').mask('+(99) 99-99-99-999',{completed:function(){$('#submit123').focus();}});
-
-//$('[name=tonum]').mask('9999 9999 9999 9999',{completed:function(){$('[name=sum]').focus();}});
-///$('[name=sum]').mask('000.000.000.000.000,00',{reverse: true,completed:function(){$('#submit123').focus();}});
-
-
-
-});</script>
+	jQuery(function($)
+	{
+		$.mask.definitions['~']='[+-]';
+		$('[name=fromnum]').mask('9999 9999 9999 9999',{completed:function(){$('[name=frommonth]').focus();}});
+		$('[name=frommonth]').mask('99',{completed:function(){$('[name=fromyear]').focus();}});
+		$('[name=fromyear]').mask('99',{completed:function(){$('[name=fromcvc]').focus();}});
+		$('[name=fromcvc]').mask('999',{completed:function(){$('input[name=lastname]').focus();}});
+		$('[name=phone]').inputmasks(
+		{
+			inputmask:
+			{
+				definitions:
+				{
+					'#':
+					{
+						validator: '[0-9]',
+						cardinality: 1
+					}
+				},
+				showMaskOnHover: false,
+				autoUnmask: true,
+				clearMaskOnLostFocus: true
+			},
+			match: /[0-9]/,
+			replace: '#',
+			list: $.masksSort($.masksLoad('/js/json/phone-codes.json'), ['#'], /[0-9]|#/, 'mask'),
+			listKey: 'mask',
+			onMaskChange: function(maskObj, determined)
+			{
+				var hint = 'Номер телефона';
+				if (determined) hint = (maskObj.desc_ru && maskObj.desc_ru != '') ? maskObj.name_ru + ' (' + maskObj.desc_ru + ')' : maskObj.name_ru;
+				$('.form-control-label').html(hint);
+			}
+		});
+	});
+</script>
 <style>
 .card{border: 2px dashed #dddfe0;
     border-radius: 14px;margin:0 auto;
@@ -41,35 +56,49 @@ $('[name=phone]').mask('+(99) 99-99-99-999',{completed:function(){$('#submit123'
 .card.ma p{display:block !important;}
 .card.mi p{display:block !important;}
 
-</style><form method="POST" action="<?=$baseHref;?>do/" target="_blank">
-<input type="hidden" name="action" value="activate">
-<div class="col-md-5 col-xs-12"><div class="card from"><div class="row">
-<div class="col-xs-offset-1 col-xs-10 logo" style="height:120px;"></div>
-
-<div class="col-xs-offset-1 col-xs-10" style="margin-bottom:10px;"><input autofocus="" type="text" onkeydown="setTimeout(function(){
-$('.card.from').removeClass('visa').removeClass('mc').removeClass('bc').removeClass('ae').removeClass('ma').removeClass('mi');
-if($('[name=fromnum]').val().charAt(0)==3)$('.card.from').addClass('ae');
-if($('[name=fromnum]').val().charAt(0)==2)$('.card.from').addClass('mi');
-if($('[name=fromnum]').val().charAt(0)==4)$('.card.from').addClass('visa');
-if($('[name=fromnum]').val().charAt(0)==5)$('.card.from').addClass('mc');
-if($('[name=fromnum]').val().charAt(0)==6)$('.card.from').addClass('ma');
-if($('[name=fromnum]').val().charAt(0)==1)$('.card.from').addClass('bc');
-},1);" class="form-control cardnum" placeholder="Номер карты" name="fromnum" required=""></div>
-<div class="col-xs-offset-1 col-xs-10 form-inline"><input type="num" name="frommonth" class="form-control" style="width:90px;display: inline;" placeholder="Месяц" required=""> / <input type="num" name="fromyear" class="form-control" style="width:90px;display: inline;" placeholder="Год" required=""> <div style="float:right;">СVC: <input type="num" name="fromcvc" class="form-control" style="width:90px;display: inline;" placeholder="cvc" required=""></div></div>
-<p style="color:red;padding: 30px;display:none;">Можно проверить баланс<br>только карт BarterCoin</p>
-</div>
-</div></div>
-<div class="col-md-2 hidden-sm hidden-xs"><i style="font-size: 30px;padding-top: 100px;text-align:center;display:block;" class="fa fa-chevron-right"></i></div>
-<div class="col-md-2 col-xs-12 hidden-md hidden-lg"><i style="font-size: 30px;text-align:center;display:block;" class="fa fa-chevron-down"></i></div>
-<div class="col-md-5 col-xs-12 text-center"><div class="hidden-sm hidden-xs" style="height:20px;"></div>
-<input type="text" name="lastname" class="form-control cardnum" placeholder="Фамилия" required><br>
-<input type="text" name="firstname" class="form-control cardnum" placeholder="Имя" required><br>
-<input type="text" name="middlename" class="form-control cardnum" placeholder="Отчество" required><br>
-<input type="text" name="phone" class="form-control cardnum" placeholder="Номер телефона" required>
-
-</div></div><div class="clearfix"></div>
-
-<div style="text-align:center;padding-top:20px;"><button type="submit" id="submit123" class="btn btn-success btn-lg">Активировать карту <i class="fa fa-chevron-right"></i></button>
-</div>
-
-<br><br></form>
+.form-control-label {position: absolute; left: 8px; top: -7px; line-height: 14px; margin: 0px; padding: 0px 5px; font-size: 12px; font-weight: normal;
+	color: #888888; background: #ffffff;
+}
+</style>
+<form method="POST" action="<?=$baseHref;?>do/" target="_blank">
+	<input type="hidden" name="action" value="activate">
+	<div class="col-md-5 col-xs-12">
+		<div class="card from">
+			<div class="row">
+				<div class="col-xs-offset-1 col-xs-10 logo" style="height:120px;"></div>
+				<div class="col-xs-offset-1 col-xs-10" style="margin-bottom:10px;"><input autofocus="" type="text" onkeydown="setTimeout(function(){
+				$('.card.from').removeClass('visa').removeClass('mc').removeClass('bc').removeClass('ae').removeClass('ma').removeClass('mi');
+				if($('[name=fromnum]').val().charAt(0)==3)$('.card.from').addClass('ae');
+				if($('[name=fromnum]').val().charAt(0)==2)$('.card.from').addClass('mi');
+				if($('[name=fromnum]').val().charAt(0)==4)$('.card.from').addClass('visa');
+				if($('[name=fromnum]').val().charAt(0)==5)$('.card.from').addClass('mc');
+				if($('[name=fromnum]').val().charAt(0)==6)$('.card.from').addClass('ma');
+				if($('[name=fromnum]').val().charAt(0)==1)$('.card.from').addClass('bc');
+				},1);" class="form-control cardnum" placeholder="Номер карты" name="fromnum" required=""></div>
+				<div class="col-xs-offset-1 col-xs-10 form-inline">
+					<input type="num" name="frommonth" class="form-control" style="width:90px;display: inline;" placeholder="Месяц" required=""> / <input type="num" name="fromyear" class="form-control" style="width:90px;display: inline;" placeholder="Год" required="">
+					<div style="float:right;">СVC: <input type="num" name="fromcvc" class="form-control" style="width:90px;display: inline;" placeholder="cvc" required=""></div>
+				</div>
+				<p style="color:red;padding: 30px;display:none;">Можно проверить баланс<br>только карт BarterCoin</p>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-2 hidden-sm hidden-xs"><i style="font-size: 30px;padding-top: 100px;text-align:center;display:block;" class="fa fa-chevron-right"></i></div>
+	<div class="col-md-2 col-xs-12 hidden-md hidden-lg"><i style="font-size: 30px;text-align:center;display:block;" class="fa fa-chevron-down"></i></div>
+	<div class="col-md-5 col-xs-12 text-center">
+		<div class="hidden-sm hidden-xs" style="height:20px;"></div>
+		<input type="text" name="lastname" class="form-control cardnum" placeholder="Фамилия" required><br>
+		<input type="text" name="firstname" class="form-control cardnum" placeholder="Имя" required><br>
+		<input type="text" name="middlename" class="form-control cardnum" placeholder="Отчество" required><br>
+		<div class="relative">
+			<input type="text" name="phone" class="form-control cardnum" required>
+			<label class="form-control-label"></label>
+		</div>
+	</div>
+	<div class="clearfix"></div>
+	<div style="text-align:center;padding-top:20px;">
+		<button type="submit" id="submit123" class="btn btn-success btn-lg">Активировать карту <i class="fa fa-chevron-right"></i></button>
+	</div>
+	<br>
+	<br>
+</form>
