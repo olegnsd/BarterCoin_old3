b2c069f0ff25f717423f3f4a2c541ced3cdaad77
jondoll <ivan.lobanov.1975@gmail.com>
2020-08-03 10:55:19 +0300

    delted files

--- qaz.php
+++
@@ -1,3 +0,0 @@
-<?php
-#$_SERVER['REMOTE_ADDR']=$_SERVER['HTTP_X_REAL_IP'];                                                                                                                                          
-phpinfo();
